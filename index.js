9c889d4ddd1967a2555b1526878a41802081014e
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2022-02-21 18:22:41 -0300

    fix method listen to work on heroku

--- index.js
+++ index.js
@@ -120,4 +120,4 @@ function writeDataIntoDatabase() {
   })
 }
 
-app.listen(5001, () => console.log('Server running on port 5001'))
+app.listen(process.env.PORT || 5001, () => console.log('Server running on port 5001'))
